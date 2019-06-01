b6aac6df84ba4642ecf55a776add2cadbff6fc08
Christian Sparks <csparks1919@hotmail.com>
2019-06-01 08:09:51 -0500

    initial commit

--- src/Main.elm
+++ src/Main.elm
@@ -1,176 +1,38 @@
-module Main exposing (Model, Msg(..), init, main, subscriptions, timeline, timelineEnd, timelineList, timelineStart, update, view)
+import module Main exposing (main)
 
-import Browser
-import Browser.Dom
-import Browser.Events
-import Browser.Navigation as Nav
+import Dict
+import Json.Encode as E
+import Lisa
 import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
-import Task
-import Time
-import Url
+
+type Msg
+    = Request String
 
 
+update : Msg -> () -> ( (), Cmd Msg )
+update msg () =
+    case msg of
+        Request s ->
+            ( ()
+            , s
+                |> Lisa.parseReplExpressionToJson { macros = Dict.empty }
+                |> out
+            )
 
--- MAIN
 
-
-main : Program () Model Msg
+main : Program E.Value () Msg
 main =
-    Browser.application
-        { init = init
-        , view = view
+    Platform.worker
+        { init = \_ -> ( (), Cmd.none )
         , update = update
-        , subscriptions = subscriptions
-        , onUrlChange = UrlChanged
-        , onUrlRequest = LinkClicked
+        , subscriptions = \_ -> incoming Request
         }
 
 
 
--- MODEL
 
 
-type alias Model =
-    { key : Nav.Key
-    , url : Url.Url
-    , width : Int
-    , now : Int
-    }
+port incoming : (String -> msg) -> Sub msg
 
 
-init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
-init flags url key =
-    (Model key url 0 0, Task.perform identity (Task.map2 InitialData Browser.Dom.getViewport Time.now ))
-
-
-
--- UPDATE
-
-
-type Msg = LinkClicked Browser.UrlRequest | UrlChanged Url.Url | Resized Int Int
-    | InitialData Browser.Dom.Viewport Time.Posix
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        LinkClicked urlRequest ->
-            case urlRequest of
-                Browser.Internal url ->
-                    ( model, Nav.pushUrl model.key (Url.toString url) )
-
-                Browser.External href ->
-                    ( model, Nav.load href )
-
-        UrlChanged url ->
-            ( { model | url = url }, Cmd.none )
-
-        Resized width height ->
-            ( { model | width = width }, Cmd.none )
-
-        InitialData viewport now ->
-            ( { model | width = truncate viewport.viewport.width, now = (Time.posixToMillis now) // 1000}, Cmd.none)
-
-
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions _ =
-    Browser.Events.onResize Resized
-
-
-
--- VIEW
-
-
-view : Model -> Browser.Document Msg
-view model =
-    { title = "Copala"
-    , body =
-        [div [class "py-6 py-md-9 w-100 text-center"]
-            [ img [ src "img/copala.svg"
-                  , class "mx-auto"
-                  , style "max-width" "1000px"
-                  , style "width" "80vw"
-                  ] []
-            , h2 [ class "mt-5 mb-5 mb-md-9 w-75 mx-auto" ] [ text "northside's student-run play festival" ]
-            , timeline model.width model.now
-            , h3 [ class "mt-5 mt-md-9" ] [ text "currently: plays!" ]
-            , p [ class "w-75 mx-auto mt-4 mb-4 mb-md-5 text-justify"
-                , style "max-width" "600px"
-            ] [ text "Copala is all about creativity and weirdness. Your play can can cover any topic and doesn’t have to even resemble a play -- just keep it school appropriate. The length should be 15-30 pages, and you can make use of the lights, curtain, and props." ]
-            , a [ class "button mx-auto", href "https://goo.gl/forms/WOlsLFdPWgRyjiFm1" ] [ text "submit a play" ]
-            ]
-        ]
-    }
-
-
-timelineList = [ ( "plays", 1553922000 ), ( "3-5 announced", 1554267600 ), ( "tryouts", 1554699600 ), ( "Copala", 1558674000 ) ]
-timelineStart = 1552398400
-timelineEnd = 1558694000
-timelineWidth = 0.7
-
-toMonth : Time.Month -> String
-toMonth month =
-  case month of
-    Time.Jan -> "January"
-    Time.Feb -> "February"
-    Time.Mar -> "March"
-    Time.Apr -> "April"
-    Time.May -> "May"
-    Time.Jun -> "June"
-    Time.Jul -> "July"
-    Time.Aug -> "August"
-    Time.Sep -> "September"
-    Time.Oct -> "October"
-    Time.Nov -> "November"
-    Time.Dec -> "December"
-
-isMd : Int -> Bool
-isMd width =
-  width >= 768
-
-timeline : Int -> Int -> Html Msg
-timeline width now =
-    let
-        posixToPercent =
-            \posix -> (posix - timelineStart) / (timelineEnd - timelineStart)
-
-        posixToPosition =
-            posixToPercent >> (*) (width |> toFloat |> (*) timelineWidth) >> String.fromFloat >> (\s -> s ++ "px")
-
-        posixToString = \posix ->
-            let truePosix = posix |> truncate |> (*) 1000 |> Time.millisToPosix
-            in
-            (toMonth <| Time.toMonth Time.utc truePosix) ++ " " ++
-            (String.fromInt <| Time.toDay Time.utc truePosix)
-    in
-    div [class "timeline"]
-        <| (if isMd width then [div [class "timeline-bar"] []] else []) ++ (
-
-            (if isMd width then
-              timelineList
-              |> (::) ("now", (toFloat now))
-            else
-              timelineList)
-            |> List.sortBy Tuple.second
-            |> List.map
-                (\(name, posix) ->
-                    div
-                        [ class "timeline-section"
-                        , class (if name == "now" then "now" else "date")
-                        , style "left" <| posixToPosition posix
-                        ]
-                        [ div [ class "timeline-text"
-                              , class (if name == "3-5 announced" || name == "now" then "top" else "bottom")
-                              ]
-                          [ p [ class "timeline-heading" ] [ text <| if name == "plays" && not (isMd width) then "plays due" else name ]
-                          , p [ class "timeline-date" ] [ text (posixToString posix) ]
-                          ]
-                        ]
-                )
-        )
-
+port out : E.Value -> Cmd msg
--- src/app.js
+++ src/app.js
@@ -1,7 +1,61 @@
-import { Elm } from './Main.elm'
+import Elm from "./Main.elm";
+import * as lisavm from "@chicode/lisa-vm";
+import * as repl from "repl";
+import { callbackify } from "util";
 import '../css/app.scss'
+import CodeMirror from 'codemirror'
 
-Elm.Main.init({
+const app = Elm.Main.init({
   node: document.getElementById('root'),
   windowWidth: window.innerWidth,
 })
+
+const processLisa = source =>
+  new Promise((res, rej) => {
+    app.ports.out.subscribe(sub);
+    app.ports.incoming.send(source);
+    function sub(result) {
+      app.ports.out.unsubscribe(sub);
+
+      if (result.status === "ok") {
+        res(result.result);
+      } else {
+        rej(result.error);
+      }
+    }
+  });
+
+const programScope = lisavm.initProgram();
+// programScope.parent.vars["-last"] = {
+//   type: "const",
+//   get value() {
+//     return rep.last || { type: "none" };
+//   }
+// };
+const rep = repl.start({
+  ignoreUndefined: true,
+  eval: callbackify(async (evalCmd, _context, _file) => {
+    try {
+      var program = await processLisa(evalCmd);
+    } catch (e) {
+      const err = new SyntaxError(e.msg);
+      throw e.recoverable ? new repl.Recoverable(err) : err;
+    }
+    let result = lisavm.values.none;
+    for (const replExpr of program) {
+      switch (replExpr.type) {
+        case "expression":
+          result = lisavm.evalExpression(programScope, replExpr.expr);
+          break;
+        case "definition":
+          programScope.vars[replExpr.name] = lisavm.evalExpression(
+            programScope,
+            replExpr.value
+          );
+      }
+    }
+    rep.last = result;
+    return result.type !== "none" ? lisavm.valueToJs(result) : undefined;
+  })
+});
+
